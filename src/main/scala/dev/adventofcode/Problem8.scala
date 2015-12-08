dfa5d05113cd519fc1951bdcd23c69fe06c78e5c
John Hungerford <jhungerford@gmail.com>
2015-12-08 07:02:01 -0700

    Problem 8 part 2.

--- src/main/scala/dev/adventofcode/Problem8.scala
+++ src/main/scala/dev/adventofcode/Problem8.scala
@@ -21,13 +21,25 @@ object Problem8 {
     case _ => 0
   }
 
+  def encodedCount(str: String): Int = str
+    .replaceAll("""\\"""", "1234")
+    .replaceAll("""\\\\""", "1234")
+    .replaceAll("""\\x[0-9a-f]{2}""", "12345")
+    .length + 4 // 4 more characters to escape the outside quotes
+
   def main(args: Array[String]) {
     val inputFile = new File(Resources.getResource("problem8/input").toURI)
 
-    val sum = Source.fromFile(inputFile).getLines().foldLeft(0) { (sum, line) =>
+    val part1Sum = Source.fromFile(inputFile).getLines().foldLeft(0) { (sum, line) =>
       sum + (Problem8.codeCount(line) - Problem8.memoryCount(line))
     }
 
-    System.out.println(s"Code - memory characters: $sum")
+    System.out.println(s"Code - memory characters: $part1Sum")
+
+    val part2Sum = Source.fromFile(inputFile).getLines().foldLeft(0) { (sum, line) =>
+      sum + (Problem8.encodedCount(line) - Problem8.codeCount(line))
+    }
+
+    System.out.println(s"Encoded - code characters: $part2Sum")
   }
 }
--- src/test/scala/dev/adventofcode/Problem8Test.scala
+++ src/test/scala/dev/adventofcode/Problem8Test.scala
@@ -5,29 +5,71 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class Problem8Test extends FlatSpec with Matchers with TableDrivenPropertyChecks {
 
-  behavior of "counts"
+  behavior of "codeCount"
 
   it should "count \"\" correctly" in {
     val str = "\"\""
     Problem8.codeCount(str) shouldEqual 2
-    Problem8.memoryCount(str) shouldEqual 0
   }
 
   it should "count \"abc\" correctly" in {
     val str = "\"abc\""
     Problem8.codeCount(str) shouldEqual 5
-    Problem8.memoryCount(str) shouldEqual 3
   }
 
   it should "count \"aaa\\\"aaa\" correctly" in {
     val str = "\"aaa\\\"aaa\""
     Problem8.codeCount(str) shouldEqual 10
-    Problem8.memoryCount(str) shouldEqual 7
   }
 
   it should "count \"\\x27\" correctly" in {
     val str = "\"\\x27\""
     Problem8.codeCount(str) shouldEqual 6
+  }
+
+
+  behavior of "memoryCount"
+
+  it should "count \"\" correctly" in {
+    val str = "\"\""
+    Problem8.memoryCount(str) shouldEqual 0
+  }
+
+  it should "count \"abc\" correctly" in {
+    val str = "\"abc\""
+    Problem8.memoryCount(str) shouldEqual 3
+  }
+
+  it should "count \"aaa\\\"aaa\" correctly" in {
+    val str = "\"aaa\\\"aaa\""
+    Problem8.memoryCount(str) shouldEqual 7
+  }
+
+  it should "count \"\\x27\" correctly" in {
+    val str = "\"\\x27\""
     Problem8.memoryCount(str) shouldEqual 1
+  }
+
+
+  behavior of "encodedCount"
+
+  it should "count \"\" correctly" in {
+    val str = "\"\""
+    Problem8.encodedCount(str) shouldEqual 6
+  }
+
+  it should "count \"abc\" correctly" in {
+    val str = "\"abc\""
+    Problem8.encodedCount(str) shouldEqual 9
+  }
+
+  it should "count \"aaa\\\"aaa\" correctly" in {
+    val str = "\"aaa\\\"aaa\""
+    Problem8.encodedCount(str) shouldEqual 16
+  }
+
+  it should "count \"\\x27\" correctly" in {
+    val str = "\"\\x27\""
+    Problem8.encodedCount(str) shouldEqual 11
   }
 }
