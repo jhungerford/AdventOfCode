3e3ab790767a167a88f4a9f3c47573f2546e35e3
John Hungerford <jhungerford@gmail.com>
2015-12-14 07:49:37 -0700

    Problem 14 part 2

--- src/main/scala/dev/adventofcode/Problem14.scala
+++ src/main/scala/dev/adventofcode/Problem14.scala
@@ -29,13 +29,32 @@ object Problem14 {
     reindeer.map{ deer => distanceAfterSeconds(deer, seconds) }.max
   }
 
+  def winningPointsAfterSeconds(reindeer: List[Reindeer], totalSeconds: Int): Int = {
+    val reindeerToPoints: Map[String, Int] = reindeer.map { deer => deer.name -> 0 }.toMap
+
+    (1 to totalSeconds).foldLeft(reindeerToPoints) { (map, second) =>
+      val deerDistance: Map[String, Int] = reindeer.map{ deer =>
+        deer.name -> distanceAfterSeconds(deer, second)
+      }.toMap
+
+      val maxDistance = deerDistance.values.max
+      val maxDistanceDeer = deerDistance.filter { case (deer, distance) => distance == maxDistance }.keys
+
+      maxDistanceDeer.foldLeft(map) { (m, deerName) =>
+        m + (deerName -> (m(deerName) + 1))
+      }
+    }.values.max
+  }
+
   def main(args: Array[String]) {
     val inputFile = new File(Resources.getResource("problem14/input").toURI)
     val reindeer: List[Reindeer] = Source.fromFile(inputFile).getLines().flatMap(parseLine).toList
 
     val seconds: Int = 2503
     val winningDistance = Problem14.winningDistanceAfterSeconds(reindeer, seconds)
+    val winningPoints = Problem14.winningPointsAfterSeconds(reindeer, seconds)
 
     System.out.println(s"Winning distance after $seconds seconds: $winningDistance")
+    System.out.println(s"Winning points after $seconds seconds: $winningPoints")
   }
 }
--- src/test/scala/dev/adventofcode/Problem14Test.scala
+++ src/test/scala/dev/adventofcode/Problem14Test.scala
@@ -5,32 +5,33 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class Problem14Test extends FlatSpec with Matchers {
 
+  private val comet: Reindeer = Reindeer("Comet", 14, 10, 127)
+  private val dancer: Reindeer = Reindeer("Dancer", 16, 11, 162)
+
   behavior of "problem 14"
 
   it should "parse a line" in {
     val line = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds."
-    val expected = Reindeer("Comet", 14, 10, 127)
-
-    Problem14.parseLine(line) shouldEqual Some(expected)
+    Problem14.parseLine(line) shouldEqual Some(comet)
   }
 
   it should "calculate distance for comet" in {
-    val comet = Reindeer("Comet", 14, 10, 127)
     Problem14.distanceAfterSeconds(comet, 1000) shouldEqual 1120
   }
 
+
   it should "calculate distance for dancer" in {
-    val comet = Reindeer("Dancer", 16, 11, 162)
-    Problem14.distanceAfterSeconds(comet, 1000) shouldEqual 1056
+    Problem14.distanceAfterSeconds(dancer, 1000) shouldEqual 1056
   }
 
   it should "calculate a winner" in {
-    val reindeer = List(
-      Reindeer("Comet", 14, 10, 127),
-      Reindeer("Dancer", 16, 11, 162)
-    )
-
+    val reindeer = List( comet, dancer )
     Problem14.winningDistanceAfterSeconds(reindeer, 1000) shouldEqual 1120
+  }
+
+  it should "calculate a winner based on points" in {
+    val reindeer = List( comet, dancer )
+    Problem14.winningPointsAfterSeconds(reindeer, 1000) shouldEqual 689
   }
 
 }
