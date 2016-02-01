d21e126ab9e1ce4673b4366cf6f6b43c69d887c9
John Hungerford <jhungerford@tendrilinc.com>
2016-02-01 09:15:15 -0700

    Problem 24.

--- src/main/scala/dev/adventofcode/Problem24.scala
+++ src/main/scala/dev/adventofcode/Problem24.scala
@@ -2,30 +2,43 @@ package dev.adventofcode
 
 object Problem24 {
 
-  case class Sleigh(group1: List[Int], group2: List[Int], group3: List[Int]) {
-    def isWeightEven: Boolean = (group1.sum == group2.sum) && (group2.sum == group3.sum)
+  def firstItem(target: Int, items: List[Int], count: Int): Option[List[Int]] = {
+    items match {
+      case item :: tail =>
+        subsetsWithSum(target - item, items diff List(item), count - 1) match {
+          case Some(list) => Some(list :+ item)
+          case None => firstItem(target, tail, count)
+        }
+      case _ => None
+    }
   }
 
-  def allArrangements(packages: List[Int]): List[Sleigh] = {
-    packages.permutations.flatMap { permutation =>
-      val stuff = for {
-        divider1 <- 1 to permutation.length - 2
-        divider2 <- divider1 + 1 to permutation.length - 1
-      } yield Sleigh(permutation.slice(0, divider1), permutation.slice(divider1, divider2), permutation.slice(divider2, permutation.length))
-      stuff
-    }.toList.filter( sleigh => sleigh.isWeightEven )
+  def subsetsWithSum(target: Int, items: List[Int], count: Int): Option[List[Int]] = {
+    if (target == 0 && count == 0) {
+      Some(List.empty)
+    } else if (count == 0) {
+      None
+    } else {
+      firstItem(target, items.filter(_ <= target), count)
+    }
+  }
+
+  def firstGroupWithEvenWeight(packages: List[Int], groups: Int, count: Int = 1): List[Int] = {
+    val weightPerGroup = packages.sum / groups
+
+    subsetsWithSum(weightPerGroup, packages, count) match {
+      case Some(subset) => subset
+      case None => firstGroupWithEvenWeight(packages, groups, count + 1)
+    }
   }
 
   def main(args: Array[String]) {
     val packages = List(1, 2, 3, 5, 7, 13, 17, 19, 23, 29, 31, 37, 41, 43, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113)
 
-    val arrangements: List[Sleigh] = allArrangements(packages)
+    val sleighThreeGroups = firstGroupWithEvenWeight(packages, 3)
+    System.out.println(s"Lightest sleigh (part1): ${sleighThreeGroups.product} - $sleighThreeGroups")
 
-    val sleighsByPackageCount: List[Sleigh] = arrangements.sortBy(sleigh => sleigh.group1.length)
-    val lightestSleigh = sleighsByPackageCount
-      .filter(sleigh => sleigh.group1.length == sleighsByPackageCount.head.group1.length)
-      .sortBy(sleigh => sleigh.group1.product).head
-
-    System.out.println(s"Lightest sleigh (part1): $lightestSleigh")
+    val sleighFourGroups = firstGroupWithEvenWeight(packages, 4)
+    System.out.println(s"Lightest sleigh (part2): ${sleighFourGroups.product}")
   }
 }
