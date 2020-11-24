2c7e18961bb54ec6e8e403c82bf28c9d200b4519
John Hungerford <jhungerford@gmail.com>
2020-11-24 13:03:07 -0700

    Day 24 part 2.

--- rust/day22/src/main.rs
+++ rust/day22/src/main.rs
@@ -3,12 +3,12 @@ use itertools::Itertools;
 // Part 1: given a list of weighted packages that need to be split into groups with equal
 // weights, what is the lowest product for the first group?  The first group should have
 // the smallest number of packages, and the sum of products of all groups should be minimized.
-fn part1(data: &Vec<i32>) -> i64 {
+fn part1(data: &Vec<i32>, groups: i32) -> i64 {
     // 1 2 3 4 5 7 8 9 10 11
     // 0 1 2 3 4 5 6 7  8  9
 
-    // 3 groups must have equal weights, so group weight = Σ weight / 3
-    let group_weight: i32 = data.iter().sum::<i32>() / 3;
+    // groups must have equal weights, so group weight = Σ weight / groups
+    let group_weight: i32 = data.iter().sum::<i32>() / groups;
 
     for n in 1..data.len() {
         let valid = data.iter().combinations(n)
@@ -31,5 +31,6 @@ fn main() {
     let data = vec![1, 2, 3, 5, 7, 13, 17, 19, 23, 29, 31, 37, 41, 43,
         53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113];
 
-    println!("Part 1: {}", part1(&data));
+    println!("Part 1: {}", part1(&data, 3));
+    println!("Part 2: {}", part1(&data, 4));
 }
